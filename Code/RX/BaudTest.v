291a58238745db83817d93af77860aa401ef7158
Divyansh Mishra <23bec016@iiitdwd.ac.in>
2025-10-31 02:28:53 +0530

    updated testbench to  verify the baud rate generator

--- Code/RX/BaudTest.v
+++ Code/RX/BaudTest.v
@@ -1,42 +1,44 @@
 `timescale 1ns/1ps
 
-module BaudTest();
+module BaudTest;
     reg rst_n;
     reg clk;
     reg [1:0] baud_rate;
     wire baud_clk;
 
-BaudGenR DUT(
-    .rst_n(rst_n),
-    .clk(clk),
-    .baud_rate(baud_rate),
-    .baud_clk(baud_clk)
-);
+    BaudGenR DUT(
+        .rst_n(rst_n),
+        .clk(clk),
+        .baud_rate(baud_rate),
+        .baud_clk(baud_clk)
+    );
 
-initial begin
-    clk = 1'b0;
-    forever begin
-        #10 clk = ~clk;
-    end 
-end
-
-initial begin
-    $dumpfile("BautGen.vcd");
-    $dumpvars(0,BaudTest);
-end
-
-integer i;
-initial begin
-    $monitor("t = %2d, rst_n = %b, clk = %b, baud_rate = %2b, baud_clk = %b", $time, rst_n, clk, baud_rate, baud_clk);
-    for (i = 0; i <= 4; i = i + 1) begin
-        baud_rate = i;
-        #(300000/(i+1));
+    initial begin
+        clk = 1'b0;
+        forever #10 clk = ~clk;
     end
-end
 
-initial begin
-    rst_n = 0;
-    #100 rst_n = 1'b1;
-end
+    initial begin
+        $dumpfile("BaudGen.vcd");
+        $dumpvars(0, BaudTest);
+    end
+
+    initial begin
+        rst_n = 0;
+        #100 rst_n = 1'b1;
+    end
+
+    integer i;
+    initial begin
+        $monitor("t = %0t ns, rst_n = %b, baud_rate = %b, baud_clk = %b", 
+                  $time, rst_n, baud_rate, baud_clk);
+
+        for (i = 0; i < 4; i = i + 1) begin
+            baud_rate = i;
+            #(3000000/(i+1)); 
+        end
+
+        $finish;
+    end
 
 endmodule
